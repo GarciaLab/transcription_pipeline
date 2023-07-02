88831516208a7865cc5fe2356dab64a2a8f65fa8
yovanBadal <ybadal@berkeley.edu>
2023-07-01 19:32:56 -0700

    Refactored nuclear detection (marke) code with private function for modularity, moved dilation iteration optimization to plateau instead of successive differences. Seems to work well now.

--- nuclear_segmentation/.ipynb_checkpoints/segmentation-checkpoint.py
+++ nuclear_segmentation/.ipynb_checkpoints/segmentation-checkpoint.py
@@ -63,37 +63,13 @@ def ellipsoid(diameter, height):
     return ellipsoid_footprint
 
 
-def iterative_peak_local_max(image, footprint, *, mask, frame_index, **kwargs):
+def _determine_num_iter(footprint):
     """
-    Find peaks in an image as coordinate list.
-
-    :param image: 2D (projected) or 3D image of a nuclear marker (usually used
-        on pre-processed images, for instance with a DoG filter).
-    :type image: Numpy array.
-    :param footprint: Footprint used during maximum dilation. This sets the minimum
-        distance between peaks, with a single maximum within the net footprint of
-        iterated maximum dilations. Can be given as a Numpy array of booleans that
-        gets used as a footprint for a single maximum dilation, as a
-        Tuple(num_iter, footprint) where the footprint is used for maximum diation
-        num_iter times, as Tuple((num_iter_lower, num_iter_upper), footprint)
-        where num_iter_lower and num_iter_upper are the lowest and highest number of
-        iterations respectively that the function will try when looking for a
-        stationary point in the number of detected maxima for each frame so that no
-        frame is over- or under-segmented.
-    :type footprint: {ndarray, Tuple(int, ndarray), Tuple((int, int), ndarray)}
-    :param mask: Mask labelling regions of interest, used to screen out spurious
-        peaks when counting detected maxima (important for searching for a
-        stationary value of the detected maxima with respect to the number of
-        iterations).
-    :type mask: Numpy array of booleans.
-    :param frame_number: Index of frame being processed, used to make warning
-        about footprint more descriptive.
-    :type frame_number: int
-    :param int averaging_window: Size of averaging window used to perform moving
-        average of number of detected peaks when selecting the 'elbow' in the
-        detection.
-    :return: Boolean mask labeling local maxima.
-    :rtype: Numpy array.
+    Parses a footprint parameter passed as a boolean numpy array (applied once),
+    as a tuple specifying the number of iterations and the boolean array setting
+    the footprint, or as a nested tuple specifying the range of number of iterations
+    to try when determining the optimal number of iterations as a tuple and a
+    boolean numpy array.
     """
     # Initialize num_iter_range to zero so that no iterations are recorded and used
     # to automatically determine a number of iterations
@@ -128,6 +104,17 @@ def iterative_peak_local_max(image, footprint, *, mask, frame_index, **kwargs):
     else:
         raise TypeError("footprint parameter must being either a tuple or numpy array.")
 
+    return footprint, num_iter, num_iter_range
+
+
+def _iterate_local_max(image, mask, iteration_params):
+    """
+    Uses a series of maximum dilations compared to the original image to select
+    local maxima inside a provided mask as per the iteration_params as returned by
+    :func:`_determine_num_iter`.
+    """
+    footprint, num_iter, num_iter_range = iteration_params
+
     # We apply a maximum dilation to the image, then compare to the original image
     # such that the only points that are selected correspond to maxima within the net
     # footprint of the dilation after the iterated application of the max filter.
@@ -149,29 +136,157 @@ def iterative_peak_local_max(image, footprint, *, mask, frame_index, **kwargs):
     for i in range(peak_mask_iter.shape[0]):
         peak_mask_iter[i] = (max_dilations_iter[i] == image) * mask
 
-    # Count number of marked maxima for each number of iterations
-    num_peaks_iter = []
-    for i in range(peak_mask_iter.shape[0]):
-        num_peaks = np.sum(peak_mask_iter[i])
-        num_peaks_iter.append(num_peaks)
+    return peak_mask_iter
 
-    # Compute successive differences between number of peaks for range of iterations
-    diff_num_peaks_iter = [s - t for s, t in zip(num_peaks_iter, num_peaks_iter[1:])]
-    diff_num_peaks_iter = np.array(diff_num_peaks_iter, dtype=float)
 
-    # Perform rolling average
+def _find_plateau(array, **kwargs):
+    """
+    Find the center of the largest plateau in a monotonically decreasing array of
+    integers, with a plateau being defines as contiguous stretches of values
+    differing by less than max_diff. Defaults to minimizing successive differences
+    if no plateau is found, where the averaging window before minimization can
+    be set by an `averaging_window` kwarg.
+    """
     try:
-        averaging_window = kwargs["averaging_window"]
+        max_diff = kwargs["max_diff"]
     except KeyError:
-        averaging_window = 3
+        max_diff = 1
 
-    diff_num_peaks_iter = ndi.uniform_filter1d(
-        diff_num_peaks_iter, size=averaging_window
-    )
+    plateau = np.zeros((array.size, array.size), dtype=bool)
 
-    # Look for stationary point
+    # This sufficient to find all plateaus because the input array is monotonically
+    # decreasing such that points within some tolerance max_diff of any given
+    # value are necessarily contiguous.
+    for i in range(array.size):
+        plateau[i] = np.abs(array - array[i]) <= max_diff
+
+    plateau_size = np.sum(plateau, axis=1)
+
+    # Check if any plateaus were found
+    num_plateaus = np.sum(plateau_size > 1)
+
+    # If no plateaus, default to minimizing successive differences
+    if num_plateaus > 0:
+        plateau_index = np.argmax(plateau_size)
+        plateau_start = np.argmax(plateau[plateau_index])
+        stationary_point = plateau_start + np.floor(
+            plateau_size[plateau_index] / 2
+        ).astype(int)
+
+    else:
+        warnings.warn(
+            "".join(
+                [
+                    "No plateau found with specified max_differences, ",
+                    "defaulting to minimzing successive differences.",
+                ]
+            )
+        )
+
+        # Compute successive differences between number of peaks for range of iterations
+        diff_array = [s - t for s, t in zip(array, array[1:])]
+        diff_array = np.array(diff_array, dtype=float)
+
+        # Perform rolling average
+        try:
+            averaging_window = kwargs["averaging_window"]
+        except KeyError:
+            averaging_window = 3
+
+        diff_array = ndi.uniform_filter1d(diff_array, size=averaging_window)
+
+        stationary_point = np.argmin(diff_array)
+
+    return stationary_point
+
+
+def _iterative_peak_warnings(
+    stationary_point, num_iter_range, frame_message, frame_end
+):
+    """
+    Constructs warnings depending on value of stationary point found during
+    optimization of footprint iteration for peak finding.
+    """
+    if stationary_point == 0:
+        warnings.warn(
+            "".join(
+                [
+                    frame_message,
+                    "Stationary point is at lower bound given for ",
+                    "number of iterations, consider extending ",
+                    "range.",
+                    frame_message_end,
+                ]
+            ),
+            stacklevel=2,
+        )
+    if stationary_point == (num_iter_range - 1):
+        warnings.warn(
+            "".join(
+                [
+                    frame_message,
+                    "Stationary point is at upper bound given for ",
+                    "number of iterations, consider extending ",
+                    "range.",
+                    frame_message_end,
+                ]
+            ),
+            stacklevel=2,
+        )
+
+    return None
+
+
+def iterative_peak_local_max(image, footprint, *, mask, frame_index, **kwargs):
+    """
+    Find peaks in an image as coordinate list.
+
+    :param image: 2D (projected) or 3D image of a nuclear marker (usually used
+        on pre-processed images, for instance with a DoG filter).
+    :type image: Numpy array.
+    :param footprint: Footprint used during maximum dilation. This sets the minimum
+        distance between peaks, with a single maximum within the net footprint of
+        iterated maximum dilations. Can be given as a Numpy array of booleans that
+        gets used as a footprint for a single maximum dilation, as a
+        Tuple(num_iter, footprint) where the footprint is used for maximum diation
+        num_iter times, as Tuple((num_iter_lower, num_iter_upper), footprint)
+        where num_iter_lower and num_iter_upper are the lowest and highest number of
+        iterations respectively that the function will try when looking for a
+        stationary point in the number of detected maxima for each frame so that no
+        frame is over- or under-segmented.
+    :type footprint: {ndarray, Tuple(int, ndarray), Tuple((int, int), ndarray)}
+    :param mask: Mask labelling regions of interest, used to screen out spurious
+        peaks when counting detected maxima (important for searching for a
+        stationary value of the detected maxima with respect to the number of
+        iterations).
+    :type mask: Numpy array of booleans.
+    :param frame_number: Index of frame being processed, used to make warning
+        about footprint more descriptive.
+    :type frame_number: int
+    :param int max_diff: Maximum difference allowed for contiguous points to be
+        considered a plateau when looking for a stationary value of the number
+        of detected nuclei with respect to the distance between peaks as set by
+        the number of iterations of the footprint. Defaults to 1.
+    :param int averaging_window: Size of averaging window used to perform moving
+        average of number of detected peaks when selecting the 'elbow' in the
+        detection. Defaults to 3, only used as fallback if plateau finding fails.
+    :return: Boolean mask labeling local maxima.
+    :rtype: Numpy array.
+    """
+    # Determine iteration parameters
+    iteration_params = _determine_num_iter(footprint)
+
+    # Find local maxima for specified range of iterations
+    peak_mask_iter = _iterate_local_max(image, mask, iteration_params)
+
+    # Count number of marked maxima for each number of iterations by summing
+    # boolean mask of peaks over all but first axis (iteration axis)
+    sum_axes = tuple(range(peak_mask_iter.ndim))[1:]
+    num_peaks_iter = np.sum(peak_mask_iter, axis=sum_axes)
+
+    # Check if frame index should be referenced by warnings if necessary
     if frame_index is not None:
-        frame_message = "Frame {0}: ".format(i)
+        frame_message = "Frame {0}: ".format(frame_index)
         frame_message_end = (
             " Note: Frame number inaccurate if chunking and parallelizing."
         )
@@ -179,48 +294,13 @@ def iterative_peak_local_max(image, footprint, *, mask, frame_index, **kwargs):
         frame_message = ""
         frame_message_end = ""
 
+    num_iter_range = iteration_params[2]
     stationary_point = 0
     if num_iter_range > 1:  # Checks if iterations were requested
-        stationary_point = np.argmin(diff_num_peaks_iter)
-
-        if stationary_point == 0:
-            warnings.warn(
-                "".join(
-                    [
-                        frame_message,
-                        "Stationary point is at lower bound given for ",
-                        "number of iterations, consider extending ",
-                        "range.",
-                        frame_message_end,
-                    ]
-                ),
-                stacklevel=2,
-            )
-        if stationary_point == (diff_num_peaks_iter.size - 1):
-            warnings.warn(
-                "".join(
-                    [
-                        frame_message,
-                        "Stationary point is at upper bound given for ",
-                        "number of iterations, consider extending ",
-                        "range.",
-                        frame_message_end,
-                    ]
-                ),
-                stacklevel=2,
-            )
-        if diff_num_peaks_iter[stationary_point] > 1:
-            warnings.warn(
-                "".join(
-                    [
-                        frame_message,
-                        "No stationary point, defaulting to mininum ",
-                        "difference between iterations.",
-                        frame_message_end,
-                    ]
-                ),
-                stacklevel=2,
-            )
+        stationary_point = _find_plateau(num_peaks_iter, **kwargs)
+        _iterative_peak_warnings(
+            stationary_point, num_iter_range, frame_message, frame_message_end
+        )
 
     peak_mask = peak_mask_iter[stationary_point]
 
@@ -360,9 +440,13 @@ def mark_frame(stack, mask, *, low_sigma, high_sigma, max_footprint, **kwargs):
     :param frame_index: Index of frame being processed, used to make warning
         about footprint more descriptive.
     :type frame_index: int, optional
+    :param int max_diff: Maximum difference allowed for contiguous points to be
+        considered a plateau when looking for a stationary value of the number
+        of detected nuclei with respect to the distance between peaks as set by
+        the number of iterations of the footprint. Defaults to 1.
     :param int averaging_window: Size of averaging window used to perform moving
         average of number of detected peaks when selecting the 'elbow' in the
-        detection.
+        detection. Defaults to 3, only used as fallback if plateau finding fails.
     :return: Array of integer labels of the same shape as image.
     :rtype: Numpy array.
     """
@@ -377,7 +461,7 @@ def mark_frame(stack, mask, *, low_sigma, high_sigma, max_footprint, **kwargs):
     except KeyError:
         frame_index = None
 
-    kwargs['frame_index'] = frame_index
+    kwargs["frame_index"] = frame_index
 
     peak_mask = iterative_peak_local_max(
         dog, footprint=max_footprint, mask=mask, **kwargs
@@ -548,9 +632,13 @@ def mark_movie(movie, mask, *, low_sigma, high_sigma, max_footprint, **kwargs):
     :param max_footprint: Footprint used by :func:`~iterative_peak_local_max`
         during maximum dilation. This sets the minimum distance between peaks.
     :type max_footprint: Numpy array of booleans.
+    :param int max_diff: Maximum difference allowed for contiguous points to be
+        considered a plateau when looking for a stationary value of the number
+        of detected nuclei with respect to the distance between peaks as set by
+        the number of iterations of the footprint. Defaults to 1.
     :param int averaging_window: Size of averaging window used to perform moving
         average of number of detected peaks when selecting the 'elbow' in the
-        detection.
+        detection. Defaults to 3, only used as fallback if plateau finding fails.
     :return: Tuple(dog, marker_coordinates, markers) where dog is the
         bandpass-filtered image, marker_coordinates is an array of the nuclear
         locations in the image indexed as per the image (this can be used for
@@ -851,9 +939,13 @@ def mark_movie_parallel(movie, mask, *, low_sigma, high_sigma, max_footprint, **
     :param max_footprint: Footprint used by :func:`~iterative_peak_local_max`
         during maximum dilation. This sets the minimum distance between peaks.
     :type max_footprint: Numpy array of booleans.
+    :param int max_diff: Maximum difference allowed for contiguous points to be
+        considered a plateau when looking for a stationary value of the number
+        of detected nuclei with respect to the distance between peaks as set by
+        the number of iterations of the footprint. Defaults to 1.
     :param int averaging_window: Size of averaging window used to perform moving
         average of number of detected peaks when selecting the 'elbow' in the
-        detection.
+        detection. Defaults to 3, only used as fallback if plateau finding fails.
     :param address: Check specified port (default is 8786) for existing
         LocalCluster to connect to.
     :type address: str or LocalCluster object
