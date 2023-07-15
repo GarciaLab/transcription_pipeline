9a3184282104d1061eadb11119451d2fe9c93af4
yovanBadal <ybadal@berkeley.edu>
2023-07-14 18:10:17 -0700

    Finished mitosis tracking. Could probably use a couple extra filters to screen out undetected divisions and clip the trace at an appropriate time, but this works for now.

--- tracking/.ipynb_checkpoints/detect_mitosis-checkpoint.py
+++ tracking/.ipynb_checkpoints/detect_mitosis-checkpoint.py
@@ -118,26 +118,55 @@ def _assign_siblings(
     search_range_mitosis,
     antiparallel_threshold,
     min_track_length,
+    image_dimensions,
+    exclude_border,
 ):
     """
     Returns an (2, n)-shape ndarray with each element along the 0-th axis corresponding
     respectively to the index in `tracked_dataframe` of a new track and to the index
-    of its sibling. This can then be used to construct lineages.
+    of its sibling. This can then be used to construct lineages. Fraction
+    `exclude_border` of the image in each border is excluded from the search for
+    new particles to assign siblings to.
     """
+    if (exclude_border is None) or (image_dimensions is None):
+        exclude_border = 0
+        image_dimensions = 0
+
+    image_dimensions = np.asarray(image_dimensions)
+    try:
+        iter(exclude_border)
+        exclude_border = np.asarray(exclude_border)
+    except TypeError:
+        pass
+
     track_first_frames, _, _ = tracks_start_end(tracked_dataframe, min_track_length)
     track_start_index = []
     siblings = []
     for i, track_start_row in track_first_frames.iterrows():
-        track_siblings = _find_sibling(
-            tracked_dataframe,
-            track_start_row,
-            pos_columns,
-            search_range_mitosis,
-            antiparallel_threshold,
+        coordinates = np.array([track_start_row[pos] for pos in pos_columns])
+
+        start_border = (
+            np.abs(coordinates - np.asarray(image_dimensions))
+            < exclude_border * image_dimensions
         )
+        end_border = np.abs(coordinates) < exclude_border * image_dimensions
+        in_border = np.any(np.array([start_border, end_border]))
+
+        if not in_border:
+            track_siblings = _find_sibling(
+                tracked_dataframe,
+                track_start_row,
+                pos_columns,
+                search_range_mitosis,
+                antiparallel_threshold,
+            )
+        else:
+            track_siblings = None
+
         if track_siblings is not None:
             track_start_index.append(i)
             siblings.append(track_siblings)
+
     sibling_array = np.array([track_start_index, siblings]).T
     return sibling_array
 
@@ -215,7 +244,9 @@ def construct_lineage(
     pos_columns,
     search_range_mitosis,
     antiparallel_threshold,
-    min_track_length=1
+    min_track_length=1,
+    image_dimensions=None,
+    exclude_border=None,
 ):
     """
     Constructs lineages in dividing nuclei by iterating over new tracks and identifying
@@ -242,6 +273,12 @@ def construct_lineage(
     :type antiparallel_threshold: scalar
     :param int min_track_length: Minimum number of frames that a new track must
         span to be considered. This helps reduce spurious detections.
+    :param image_dimensions: Image dimension in each respective coordinate of
+        `pos_columns`.
+    :type image_dimensions: Array of integers.
+    :param exclude_border: Fraction of border in each position coordinate
+        to exclude when searching for new particles to assign siblings to.
+    :type exclude_border: float or array of floats for each coordinate.
     :return: Copy of `tracked_dataframe` with particle identities corresponding to
         lineage and an added column with parent identities.
     :rtype: pandas DataFrame
@@ -255,6 +292,8 @@ def construct_lineage(
         search_range_mitosis,
         antiparallel_threshold,
         min_track_length,
+        image_dimensions,
+        exclude_border,
     )
 
     # Assign parent by indexing over dataframe
@@ -266,4 +305,62 @@ def construct_lineage(
     # Add parents to sibling pairs
     _add_parent(mitosis_dataframe, sibling_array, parent_index_array)
 
+    # Relabel all particles with the label assigned at the first frame
+    # by the sibling sorting
+    for _, track in mitosis_dataframe.groupby("particle"):
+        particle = track["particle"].values[0]
+        orphan = track["parent"].notnull().sum() == 0
+        if not orphan:
+            mitosis_dataframe.loc[
+                mitosis_dataframe["particle"] == particle, "parent"
+            ] = track["parent"][track["parent"].notnull()].values[0]
+
     return mitosis_dataframe
+
+
+def tracks_to_napari(viewer, dataframe, name='tracks', output=False):
+    """
+    Uses tracking and lineage information in `mitosis_dataframe` to construct
+    tracks datastructures compatible with napari tracks layer. If output is requested,
+    will also return these data structures.
+
+    :param viewer: napari viewer.
+    :type viewer: napari viewer object
+    :param dataframe: DataFrame of measured features after tracking with
+        :func:`~link_dataframe` and lineage construction with `construct_lineage`.
+    :type dataframe: pandas DataFrame
+    :param bool output: If True, returns the napari-compatible datastructures used
+        to visualize tracks.
+    :return: If `output = True`, returns lineage dictionary and parent properties
+        used to visualize tracks in napari.
+    :rtype: {Tuple, None}
+    """
+    mitosis_dataframe = dataframe.copy()
+    mitosis_dataframe['frame'] = mitosis_dataframe['frame'] - 1
+    
+    tracks = mitosis_dataframe.loc[:, ["particle", "frame", "z", "y", "x"]].to_numpy()
+    
+    lineage = {}
+    for _, family in mitosis_dataframe.groupby("parent"):
+        parent = (family["parent"].values)[0]
+        children = family["particle"].to_list()
+        lineage[parent] = children
+
+    properties = {
+        "parent": [
+            mitosis_dataframe.loc[
+                mitosis_dataframe["particle"] == idx, "parent"
+            ].values.fillna(0)[0]
+            for idx in tracks[:, 0]
+        ]
+    }
+
+    viewer.add_tracks(tracks, properties=properties, graph=lineage, name=name)
+
+    if output:
+        out = lineage, properties
+
+    else:
+        out = None
+
+    return out
